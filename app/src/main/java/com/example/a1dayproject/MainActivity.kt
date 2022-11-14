9ab9d8ed066c7c81865a0cecb6e6eff0605919a2
divtab <97197155+divtab@users.noreply.github.com>
2022-11-14 23:26:58 +0900

    commit オプションicon押下で編集画面へintent

--- app/src/main/java/com/example/a1dayproject/MainActivity.kt
+++ app/src/main/java/com/example/a1dayproject/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.a1dayproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //メニューを初めて表示するときに一度だけ呼び出される。
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+    //オプションメニューの項目が選択されたときに呼ばれる。
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //戻り値用の変数を初期値trueで用意。
+        var returnVal = true
+        var intent: Intent
+        //item.itemIdは、選択されたオプションメニューのid
+        when(item.itemId){
+            R.id.action_editMode -> {
+                intent = Intent(this@MainActivity, EditMode::class.java)
+                startActivity(intent)
+            }
+            else ->
+                returnVal = super.onOptionsItemSelected(item)
+        }
+        return  returnVal
     }
 }
