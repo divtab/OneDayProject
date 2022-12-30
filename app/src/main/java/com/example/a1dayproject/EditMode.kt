e9e139f32122114caac646472a43ba9b6af1dca7
divtab <97197155+divtab@users.noreply.github.com>
2022-12-31 01:12:56 +0900

    EditMode
    TextEdit空欄時、
    保存変更ボタンを変更ボタンを
    使用不可にする。

--- app/src/main/java/com/example/a1dayproject/EditMode.kt
+++ app/src/main/java/com/example/a1dayproject/EditMode.kt
@@ -1,11 +1,18 @@
 package com.example.a1dayproject
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -17,6 +24,7 @@ import com.example.a1dayproject.db.UserEntity
 class EditMode : AppCompatActivity(),RecyclerViewAdapter.RowClickListener{
     lateinit var recyclerViewAdapter: RecyclerViewAdapter
     lateinit var viewModel: MainActivityViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,38 +48,66 @@ class EditMode : AppCompatActivity(),RecyclerViewAdapter.RowClickListener{
             recyclerViewAdapter.setListData(ArrayList(it))
             recyclerViewAdapter.notifyDataSetChanged()
         })
+        val saveButton = findViewById<Button>(R.id.saveBtn)
+        val project  = findViewById<EditText>(R.id.etProject)
+
+
+//      テキストエディット空欄時、保存変更ボタンを使用不可にする。
+        project.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+            override fun afterTextChanged(p0: Editable?) {
+                saveButton.isEnabled = p0?.toString().equals("") != true
+            }
+        })
+
 
         //保存ボタン押下時
-        val saveButton = findViewById<Button>(R.id.saveBtn)
         saveButton.setOnClickListener {
 
             val project  = findViewById<EditText>(R.id.etProject)
-            if(saveButton.text.equals("保存")) {
-                val user = UserEntity(0, project.text.toString(),false)
-                viewModel.insertUserInfo(user)
+            if(project.text.toString() == ""){
+                   val toast = Toast.makeText(this,"空欄では登録できません。",Toast.LENGTH_LONG)
+                   toast.show()
             }else {
-                val user = UserEntity(project.getTag(project.id).toString().toInt(), project.text.toString(),false)
-                viewModel.updateUserInfo(user)
-                saveButton.setText("保存")
+                if (saveButton.text.equals("保存")) {
+                    val user = UserEntity(0, project.text.toString(), false)
+                    viewModel.insertUserInfo(user)
+                } else {
+                    val user = UserEntity(
+                        project.getTag(project.id).toString().toInt(),
+                        project.text.toString(),
+                        false
+                    )
+                    viewModel.updateUserInfo(user)
+                    saveButton.text = "保存"
+                }
+                project.setText("")
             }
-            project.setText("")
         }
+
+
     }
 
 
+
     override fun onDeleteUserClickListener(user: UserEntity) {
+        val project  = findViewById<EditText>(R.id.etProject)
+        val saveButton = findViewById<Button>(R.id.saveBtn)
+        saveButton.text = "保存"
+        project.setText("")
+
         viewModel.deleteUserInfo(user)
     }
 
     override fun onItemClickListener(user: UserEntity) {
         val project  = findViewById<EditText>(R.id.etProject)
         val saveButton = findViewById<Button>(R.id.saveBtn)
-
-        //
-
         project.setText(user.name)
         project.setTag(project.id, user.id)
-        saveButton.setText("変更")
+        saveButton.text = "変更"
     }
 
     //optionBar 押下処理
